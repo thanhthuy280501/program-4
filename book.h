79cbd15fe50330cfef2a35d640ffe49364c087ab
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-12-09 00:25:01 -0500

    Update book.h

--- book.h
+++ book.h
@@ -1,28 +1,31 @@
 #ifndef BOOK_H
 #define BOOK_H
-
+#include "LibraryItem.h"
 #include <string>
 #include <iostream>
 using namespace std;
 
-class Book {
+class Book : : public LibraryItem {
 private:
     string title;
     string author;
     string ISBN;
     string publisher;
+    std::string author;
 
 public:
     // Constructors and Destructor
     Book();
-    Book(string t, string a, string i, string p);
-    ~Book();
+    Book(const std::string &t, const std::string &a, const std::string &i, const std::string &p, const std::string &s = "available");
+    ~Book() {}
 
-    // Getters
+    // Getters and Setter for Author
     string getTitle() const;
     string getAuthor() const;
     string getISBN() const;
     string getPublisher() const;
+    std::string getAuthor() const;
+    void setAuthor(const std::string &a);
 
     // Setters
     void setTitle(const string &t);
@@ -40,6 +43,12 @@ public:
     bool operator==(const Book &other) const;
     bool operator!=(const Book &other) const;
     friend ostream &operator<<(ostream &os, const Book &book);
+
+    // Overridden Functions
+    void displayDetails() const override;
+    void borrowItem() override;
+    void returnItem() override;
 };
+
 
 #endif

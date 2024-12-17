72d93d1fb52d679d489f167128f6af018372cbc2
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-12-16 23:38:55 -0500

    Update book.cpp

--- book.cpp
+++ book.cpp
@@ -1,89 +1,28 @@
-#include "Book.h"
-#include <algorithm>
-#include <iostream>
+#ifndef BOOK_H
+#define BOOK_H
 
-Book::Book() : title(""), author(""), ISBN(""), publisher("") {}
+#include "LibraryItem.h"
 
-Book::Book(string t, string a, string i, string p)
-    : title(t), author(a), ISBN(i), publisher(p) {}
+class Book : public LibraryItem {
+private:
+    std::string author;
 
-Book::~Book() {}
-// Constructors
-Book::Book() : LibraryItem(), author("") {}
-Book::Book(const std::string &t, const std::string &a, const std::string &i, const std::string &p, const std::string &s)
-    : LibraryItem(t, i, p, s), author(a) {}
+public:
+    // Constructors
+    Book();
+    Book(const std::string &t, const std::string &i, const std::string &p, const std::string &a, const std::string &s = "available");
 
-// Getters
-string Book::getTitle() const { return title; }
-string Book::getAuthor() const { return author; }
-string Book::getISBN() const { return ISBN; }
-string Book::getPublisher() const { return publisher; }
+    // Overridden Virtual Functions
+    virtual void displayDetails() const override;
+    virtual void borrowItem() override;
+    virtual void returnItem() override;
+    virtual void updateItem() override;
+    virtual void newItem() override;
+    virtual std::string getType() const override;
 
-// Setters
-void Book::setTitle(const string &t) { title = t; }
-void Book::setAuthor(const string &a) { author = a; }
-void Book::setISBN(const string &i) { ISBN = i; }
-void Book::setPublisher(const string &p) { publisher = p; }
+    // Getters and Setters
+    virtual std::string getAuthor() const override;
+    virtual void setAuthor(const std::string &a) override;
+};
 
-// String Manipulation
-void Book::convertTitleToUppercase() {
-    transform(title.begin(), title.end(), title.begin(), ::toupper);
-}
-
-void Book::convertTitleToLowercase() {
-    transform(title.begin(), title.end(), title.begin(), ::tolower);
-}
-
-void Book::convertAuthorToUppercase() {
-    transform(author.begin(), author.end(), author.begin(), ::toupper);
-}
-
-void Book::convertAuthorToLowercase() {
-    transform(author.begin(), author.end(), author.begin(), ::tolower);
-}
-
-// Overloaded Operators
-bool Book::operator==(const Book &other) const { return ISBN == other.ISBN; }
-bool Book::operator!=(const Book &other) const { return ISBN != other.ISBN; }
-ostream &operator<<(ostream &os, const Book &book) {
-    os << book.title << " by " << book.author << ", ISBN: " << book.ISBN
-       << ", Publisher: " << book.publisher;
-    return os;
-}
-void sortLibrary(vector<Book> &library) {
-    sort(library.begin(), library.end(), [](const Book &a, const Book &b) {
-        return a.getISBN() < b.getISBN();
-    });
-}
-Book *searchBookByISBN(vector<Book> &library, const string &isbn, int index = 0) {
-    if (index >= library.size()) return nullptr;
-    if (library[index].getISBN() == isbn) return &library[index];
-    return searchBookByISBN(library, isbn, index + 1);
-}
-// Overridden Functions
-void Book::displayDetails() const {
-    std::cout << "Book Details:\n"
-              << "Title: " << title << "\n"
-              << "Author: " << author << "\n"
-              << "ISBN: " << ISBN << "\n"
-              << "Publisher: " << publisher << "\n"
-              << "Status: " << status << "\n";
-}
-void Book::borrowItem() {
-    if (status == "available") {
-        status = "borrowed";
-        std::cout << "Book borrowed successfully.\n";
-    } else {
-        std::cout << "Book is already borrowed.\n";
-    }
-}
-
-void Book::returnItem() {
-    if (status == "borrowed") {
-        status = "available";
-        std::cout << "Book returned successfully.\n";
-    } else {
-        std::cout << "Book is already available.\n";
-    }
-}
-
+#endif

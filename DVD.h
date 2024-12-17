0fb78006eb464fe37be80d1c0bbfc1750b253b44
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-12-16 23:40:01 -0500

    Update DVD.h

--- DVD.h
+++ DVD.h
@@ -9,21 +9,23 @@ private:
     int runtime; // in minutes
 
 public:
-    // Constructors and Destructor
+    // Constructors
     DVD();
-    DVD(const std::string &t, const std::string &d, int r, const std::string &i, const std::string &p, const std::string &s = "available");
-    ~DVD() {}
+    DVD(const std::string &t, const std::string &i, const std::string &p, const std::string &d, int r, const std::string &s = "available");
+
+    // Overridden Virtual Functions
+    virtual void displayDetails() const override;
+    virtual void borrowItem() override;
+    virtual void returnItem() override;
+    virtual void updateItem() override;
+    virtual void newItem() override;
+    virtual std::string getType() const override;
 
     // Getters and Setters
-    std::string getDirector() const;
-    void setDirector(const std::string &d);
-    int getRuntime() const;
-    void setRuntime(int r);
-
-    // Overridden Functions
-    void displayDetails() const override;
-    void borrowItem() override;
-    void returnItem() override;
+    virtual std::string getDirector() const override;
+    virtual int getRuntime() const override;
+    virtual void setDirector(const std::string &d) override;
+    virtual void setRuntime(int r) override;
 };
 
 #endif

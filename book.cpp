219bbcd90609656b199642265c60f9f09cc34370
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-12-16 23:39:33 -0500

    Update book.cpp

--- book.cpp
+++ book.cpp
@@ -1,28 +1,82 @@
-#ifndef BOOK_H
-#define BOOK_H
+#include "Book.h"
+#include <iostream>
+using namespace std;
 
-#include "LibraryItem.h"
+// Default Constructor
+Book::Book() : LibraryItem(), author("") {}
 
-class Book : public LibraryItem {
-private:
-    std::string author;
+// Parameterized Constructor
+Book::Book(const string &t, const string &i, const string &p, const string &a, const string &s)
+    : LibraryItem(t, i, p, s), author(a) {}
 
-public:
-    // Constructors
-    Book();
-    Book(const std::string &t, const std::string &i, const std::string &p, const std::string &a, const std::string &s = "available");
+// Display Book Details
+void Book::displayDetails() const {
+    cout << "Type: Book\n"
+         << "Title: " << title << "\n"
+         << "Author: " << author << "\n"
+         << "ISBN: " << ISBN << "\n"
+         << "Publisher: " << publisher << "\n"
+         << "Status: " << status << endl;
+}
 
-    // Overridden Virtual Functions
-    virtual void displayDetails() const override;
-    virtual void borrowItem() override;
-    virtual void returnItem() override;
-    virtual void updateItem() override;
-    virtual void newItem() override;
-    virtual std::string getType() const override;
+// Borrow a Book
+void Book::borrowItem() {
+    if (status == "available") {
+        status = "borrowed";
+        cout << "Book '" << title << "' has been borrowed." << endl;
+    } else {
+        cout << "Book '" << title << "' is already borrowed." << endl;
+    }
+}
 
-    // Getters and Setters
-    virtual std::string getAuthor() const override;
-    virtual void setAuthor(const std::string &a) override;
-};
+// Return a Book
+void Book::returnItem() {
+    if (status == "borrowed") {
+        status = "available";
+        cout << "Book '" << title << "' has been returned." << endl;
+    } else {
+        cout << "Book '" << title << "' was not borrowed." << endl;
+    }
+}
 
-#endif
+// Update Book Details
+void Book::updateItem() {
+    cout << "Updating Book Information:" << endl;
+    cout << "Enter new title: ";
+    getline(cin, title);
+    cout << "Enter new author: ";
+    getline(cin, author);
+    cout << "Enter new publisher: ";
+    getline(cin, publisher);
+    cout << "Enter new status (available/borrowed): ";
+    getline(cin, status);
+}
+
+// Add a New Book
+void Book::newItem() {
+    cout << "Adding New Book:" << endl;
+    cout << "Enter title: ";
+    getline(cin, title);
+    cout << "Enter author: ";
+    getline(cin, author);
+    cout << "Enter ISBN: ";
+    getline(cin, ISBN);
+    cout << "Enter publisher: ";
+    getline(cin, publisher);
+    status = "available";
+}
+
+// Get Type
+string Book::getType() const {
+    return "book";
+}
+
+// Get Author
+string Book::getAuthor() const {
+    return author;
+}
+
+// Set Author
+void Book::setAuthor(const string &a) {
+    author = a;
+}
